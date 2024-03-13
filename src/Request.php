174cb1ee78fedf2b29f5585ca710238a8cdc9675
ralphxp <raphapanchi@gmail.com>
2024-03-13 20:33:42 +0100

    modified:   src/Request.php

--- src/Request.php
+++ src/Request.php
@@ -4,43 +4,6 @@ namespace Codx\Comic;
 use Symfony\Component\HttpFoundation\Request as Requests;
 
 class Request extends Requests{
-    private $method;
-    public $uri;
-    // public $headers;
-    private $queryParams;
-    private $body;
-
-    public function __construct()
-    {
-        $this->method = $_SERVER['REQUEST_METHOD'];
-        $this->uri = $_SERVER['REQUEST_URI'];
-        // $this->headers = getallheaders();
-        $this->queryParams = $_GET;
-        $this->body = file_get_contents('php://input');
-    }
-
-    public function getMethod()
-    {
-        return $this->method;
-    }
-
-    public function getUri()
-    {
-        return $this->uri;
-    }
-
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
-
-    public function getQueryParams()
-    {
-        return $this->queryParams;
-    }
-
-    public function getBody()
-    {
-        return $this->body;
-    }
+    
+    
 }
