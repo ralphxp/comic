8ebbc5001aedb8637e29d14790b6bb8baf1de699
ralphxp <raphapanchi@gmail.com>
2024-05-17 07:23:41 +0100

    modified:   composer.json modified:   composer.lock modified:   src/Core.php

--- src/Core.php
+++ src/Core.php
@@ -11,15 +11,18 @@ class Core{
 // 
     public static function runApp()
     {
+        $dotenv = \Dotenv\Dotenv::createImmutable(getcwd());
+        $dotenv->load();
+
         $capsule = new Capsule;
         $capsule->addConnection([
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'database' => 'rental_house',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'driver' => $_ENV['DB_DRIVER'],
+            'host' => $_ENV['DB_HOST'],
+            'database' => $_ENV['DB_NAME'],
+            'username' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASS'],
+            'charset' => $_ENV['DB_CHARSET'],
+            'collation' => $_ENV['DB_COLLATION'],
             'prefix' => '',
         ]);
 
